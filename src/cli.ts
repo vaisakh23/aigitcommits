e3aff29bfe50634e37793a89a62f2ae2d5e36fc1
Vaisakh <vaisakh@richkenmedia.com>
2023-06-06 14:44:39 +0530

    yargs implement

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,29 @@
 #!/usr/bin/env node
-import fs from "fs";
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
 
-console.log("git commit cli");
+yargs(hideBin(process.argv))
+  .command(
+    "$0",
+    "gitcommits",
+    () => {},
+    (args) => {
+        console.log('default command')
+    }
+  )
+  //   .command('serve [port]', 'start the server', (yargs) => {
+  //     return yargs
+  //       .positional('port', {
+  //         describe: 'port to bind on',
+  //         default: 5000
+  //       })
+  //   }, (argv) => {
+  //     if (argv.verbose) console.info(`start server on :${argv.port}`)
+  //   })
+  //   .option('verbose', {
+  //     alias: 'v',
+  //     type: 'boolean',
+  //     description: 'Run with verbose logging'
+  //   })
+  .help()
+  .parse();
