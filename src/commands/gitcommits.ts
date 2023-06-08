b453140d13c0c3839f6e05c9a23ec6d44c5da520
vaisakh <vaisakhsindhu11@gmail.com>
2023-06-08 22:47:24 +0530

    get git diff and send to openai

--- src/commands/gitcommits.ts
+++ src/commands/gitcommits.ts
@@ -1,5 +1,20 @@
+import { Configuration, OpenAIApi } from "openai";
 import { getStagedChanges } from "../utils/getStagedChanges.js";
 
-export default () => {
-    getStagedChanges()
+export default async () => {
+  const gitDiff = await getStagedChanges();
+  const openai = new OpenAIApi(
+    new Configuration({ apiKey: process.env.OPENAI_API_KEY })
+  );
+  openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: `Generate a concise git commit message written in present tense for the following code diff.
+        Your entire response will be passed directly into git commit.`,
+      },
+      { role: "user", content: gitDiff },
+    ],
+  });
 };
--- src/utils/getStagedChanges.ts
+++ src/utils/getStagedChanges.ts
@@ -1,6 +1,6 @@
 import { execa } from "execa";
 
 export const getStagedChanges = async () => {
-  const { stdout: changes } = await execa("git", ["diff", "--cached"]);
-  return changes
+  const { stdout: gitDiff } = await execa("git", ["diff", "--cached"]);
+  return gitDiff;
 };
