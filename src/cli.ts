39c66664b59e841b69cf3d2bfe8a6d38533bb7ac
vaisakh <vaisakhsindhu11@gmail.com>
2023-06-17 16:58:58 +0530

    config command

--- src/cli.ts
+++ src/cli.ts
@@ -2,29 +2,36 @@
 import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
 import gitcommits from "./commands/gitcommits.js";
+import config from "./commands/config.js";
 
 yargs(hideBin(process.argv))
+  .usage("usage: $0 <command> [<options>]")
   .command(
     "$0",
     "gitcommits",
     () => {},
     (args) => {
-        gitcommits()
+      gitcommits();
     }
   )
-  //   .command('serve [port]', 'start the server', (yargs) => {
-  //     return yargs
-  //       .positional('port', {
-  //         describe: 'port to bind on',
-  //         default: 5000
-  //       })
-  //   }, (argv) => {
-  //     if (argv.verbose) console.info(`start server on :${argv.port}`)
-  //   })
-  //   .option('verbose', {
-  //     alias: 'v',
-  //     type: 'boolean',
-  //     description: 'Run with verbose logging'
-  //   })
+  .command(
+    "config [key] [value]",
+    "Manage configuration",
+    (yargs) => {
+      return yargs
+        .option("get", {
+          describe: "get api key",
+        })
+        .positional("key", {
+          type: "string",
+        })
+        .positional("value", {
+          type: "string",
+        });
+    },
+    config
+  )
+  .strict()
   .help()
+  .alias("help", "h")
   .parse();
---
+++ src/commands/config.ts
@@ -0,0 +1,6 @@
+const config = (args: any) => {
+  console.log(args);
+  return 
+};
+
+export default config;
