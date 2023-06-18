c7d51e3fd5ac9322ebf99c403b131a8189faeaef
vaisakh <vaisakhsindhu11@gmail.com>
2023-06-18 16:49:35 +0530

    error handling in generateCommitMessage function.

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -1,22 +1,37 @@
 import { Configuration, OpenAIApi } from "openai";
 import { generatePrompt } from "./prompt.js";
+import { getConfig } from "./config.js";
+import { CmdLogs } from "./cmdLog.js";
 
-const openai = new OpenAIApi(
-  new Configuration({
-    apiKey: process.env.OPEN_API_KEY,
-  })
-);
+const initOpenAi = (config: any) => {
+  const openai = new OpenAIApi(
+    new Configuration({
+      apiKey: config.OPENAI_KEY,
+    })
+  );
+  return openai;
+};
 
 export const generateCommitMessage = async (gitDiff: string) => {
-  const commit_msg = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content: generatePrompt(),
-      },
-      { role: "user", content: gitDiff },
-    ],
-  });
-  return commit_msg;
+  try {
+    const config = await getConfig("OPENAI_KEY");
+    const openai = initOpenAi(config);
+    const response = await openai?.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: generatePrompt(),
+        },
+        { role: "user", content: gitDiff },
+      ],
+    });
+    return response?.data.choices[0].message?.content;
+  } catch (err: any) {
+    if (err.response) {
+      await CmdLogs.openaiError(err.response.data.error);
+    } else {
+      CmdLogs.errMsg("Failed to connect OpenAI");
+    }
+  }
 };
