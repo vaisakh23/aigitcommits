07927ae1d1263a38fbc17707f1afef8640feeae7
vaisakh <vaisakhsindhu11@gmail.com>
2023-06-18 16:48:18 +0530

    cmd respones

--- src/utils/cmdLog.ts
+++ src/utils/cmdLog.ts
@@ -1,4 +1,27 @@
+import { getConfig } from "./config.js";
+
 export class CmdLogs {
+  static commitMsg(msg: string) {
+    console.log(msg);
+    process.exit(1);
+  }
+
+  static result(msg: string) {
+    console.log(msg);
+    process.exit(1);
+  }
+
+  static info(msg: string) {
+    console.log(msg);
+    process.exit(1);
+  }
+
+  static async openaiError(err: any) {
+    const { OPENAI_KEY } = await getConfig("OPENAI_KEY");
+    console.log(`${err.code} \nyour key ${OPENAI_KEY}`);
+    process.exit(1);
+  }
+
   static errMsg(msg: string) {
     console.log(msg);
     process.exit(1);
