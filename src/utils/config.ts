18b75c1c1944e38cc19ece51e6e91cb4c527c473
vaisakh <vaisakhsindhu11@gmail.com>
2023-06-18 13:06:09 +0530

    config utils

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,23 +1,66 @@
 import fs from "fs/promises";
 import path from "path";
 import os from "os";
+import { CmdLogs } from "./cmdLog.js";
 
 const configPath = path.join(os.homedir(), ".gitcommits");
 
-export const getConfig = async () => {
-  const fileExist = await fs.lstat(configPath).then(
+const configParsers = {
+  OPENAI_KEY(value: string) {
+    if (!value) {
+      CmdLogs.errMsg(
+        "Please set your OpenAI API key via `gitcommits config OPENAI_KEY=<your token>`"
+      );
+    }
+    if (!value.startsWith('sk-')) {
+      CmdLogs.errMsg("Please provide a valid Api key.")
+    }
+    return value;
+  },
+};
+
+type ConfigKeys = keyof typeof configParsers;
+
+const fileExist = (configPath: string) =>
+  fs.lstat(configPath).then(
     () => true,
     () => false
   );
-  if (!fileExist) {
+
+const readConfigFile = async () => {
+  const configExist = await fileExist(configPath);
+  if (!configExist) {
     return Object.create(null);
   }
   const config = await fs.readFile(configPath, "utf8");
   return JSON.parse(config);
 };
 
+export const validConfigKey = (key: PropertyKey) => {
+  const { hasOwnProperty } = Object.prototype;
+  return hasOwnProperty.call(configParsers, key);
+};
+
+export const getConfig = async (keys?: ConfigKeys | ConfigKeys[]) => {
+  if (typeof keys === "string") keys = [keys];
+  if (!keys) keys = Object.keys(configParsers) as ConfigKeys[];
+
+  const config = await readConfigFile();
+  const parsedConfig: Record<string, unknown> = {};
+
+  for (const key of keys) {
+    const parser = configParsers[key];
+    const value = config[key];
+    parsedConfig[key] = parser(value);
+  }
+  return parsedConfig;
+};
+
 export const setConfigs = async (key: string, value: string) => {
-  const config = await getConfig();
-  (config as any)[key] = value;
+  const config = await readConfigFile();
+  if (!validConfigKey(key)) {
+    CmdLogs.errMsg(`Invalid config property: ${key}`);
+  }
+  config[key] = value;
   await fs.writeFile(configPath, JSON.stringify(config), "utf8");
 };
