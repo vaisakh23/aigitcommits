6b910dddd0f3dbfd62d6a22d9e6b83321eb5c3c2
vaisakh <vaisakhsindhu11@gmail.com>
2023-06-18 16:46:00 +0530

    Add error handling for when no staged changes or commit messages are found

--- src/commands/gitcommits.ts
+++ src/commands/gitcommits.ts
@@ -1,9 +1,17 @@
+import { CmdLogs } from "../utils/cmdLog.js";
 import { getStagedChanges } from "../utils/getStagedChanges.js";
 import { generateCommitMessage } from "../utils/openai.js";
 
 const gitcommits = async (args: any) => {
   const gitDiff = await getStagedChanges();
+  if (!gitDiff) {
+    CmdLogs.errMsg("No staged changes found. Stage your changes manually.");
+  }
   const commit_msg = await generateCommitMessage(gitDiff);
-  console.log(commit_msg.data.choices[0].message?.content);
+  if (!commit_msg) {
+    CmdLogs.info("No commit messages were generated. Try again.");
+  } else {
+    CmdLogs.commitMsg(commit_msg)
+  }
 };
 export default gitcommits;
