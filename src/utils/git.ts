214a133f8fad1eef684c9278695f32a0d7555a98
vaisakh <vaisakhsindhu11@gmail.com>
2023-07-11 21:43:09 +0530

    "Update package version to 1.3.1 and exclude swagger_output.json from diff"

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -2,9 +2,12 @@ import { execa } from "execa";
 
 const excludeFromDiff = (path: string) => `:(exclude)${path}`;
 
-const filesToExclude = ["package-lock.json", "pnpm-lock.yaml", "*.lock"].map(
-  excludeFromDiff
-);
+const filesToExclude = [
+  "package-lock.json",
+  "pnpm-lock.yaml",
+  "*.lock",
+  "swagger_output.json",
+].map(excludeFromDiff);
 
 export const getStagedChanges = async () => {
   const { stdout: gitDiff } = await execa("git", [
